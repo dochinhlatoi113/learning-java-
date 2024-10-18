23dbd0288cba48a54e86af47f045252c4903b97b
Bùi Khương <dochinhlatoi113@gmail.com>
2024-10-18 14:41:44 +0700

    learning java constant

---
+++ Constant.java
@@ -0,0 +1,7 @@
+public class Constant {
+    public static void main(String[] args) {
+        final int test = 1;
+         //test = 200; sẽ bị báo lỗi do dùng final hằng số
+
+    }
+}
--- Datatype.java
+++ Datatype.java
@@ -1,5 +1,10 @@
 public class Datatype {
     // có 2 kiểu dử liệu 
-    // - nguyên thủy :  int,double,short,long,float,boolen
-    // - đối tượng : array,số thực,chuổi
+    // - nguyên thủy :  int,double,short,long,float,boolen => tất cả được viết thường
+    // - đối tượng : Array,Double,String =>  chử cái đầu viết hoa
+    String $name = "java";
+    int $age = 100;
+    double price = 5.500;
+    boolean status = true;
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 }
