1bf3baee3bdb7ba5e70e09a3fdc08f7d9b393b53
Bùi Khương <dochinhlatoi113@gmail.com>
2024-10-18 14:33:20 +0700

    learning java datatype

---
+++ Datatype.java
@@ -0,0 +1,5 @@
+public class Datatype {
+    // có 2 kiểu dử liệu 
+    // - nguyên thủy :  int,double,short,long,float,boolen
+    // - đối tượng : array,số thực,chuổi
+}
---
+++ JavaVariables.java
@@ -0,0 +1,6 @@
+public class JavaVariables {
+  public static void main(String[] args) {
+    String $name = "this is Java Variables";
+    System.out.println($name);
+  }
+}
